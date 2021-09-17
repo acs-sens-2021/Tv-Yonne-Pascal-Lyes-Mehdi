1711aed35f2eca87f53453c678eeceebf932856c
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-17 10:56:00 +0200

    ajout js mode light

---
+++ js/mode.js
@@ -0,0 +1,6 @@
+let link = document.querySelector(".link");
+let lightbtn = document.querySelector(".lightmode");
+
+lightbtn.addEventListener("click", () =>{
+    link.href = "css/lightmode.css";
+})
