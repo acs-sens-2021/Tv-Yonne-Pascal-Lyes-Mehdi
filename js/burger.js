9316a7c27c2d76241cee6bd05a15670381f28595
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-17 07:12:30 +0200

    ajout fermeture menu au click sur le main

--- js/burger.js
+++ js/burger.js
@@ -2,6 +2,7 @@ let burger = document.querySelector(".burger");
 let menu = document.querySelector(".lien");
 let nav = document.querySelector(".nav-main");
 let closebtn = document.querySelector(".closebtn");
+let body = document.querySelector("main");
 
 burger.addEventListener("click", () => {
     menu.style.display = "flex";
@@ -10,6 +11,12 @@ burger.addEventListener("click", () => {
 });
 
 closebtn.addEventListener("click", () => {
+    menu.style.display = "none";
+    closebtn.style.display = "none";
+    burger.style.display = "flex";
+});
+
+body.addEventListener("click", () => {
     menu.style.display = "none";
     closebtn.style.display = "none";
     burger.style.display = "flex";
