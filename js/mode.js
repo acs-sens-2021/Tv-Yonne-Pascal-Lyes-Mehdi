40576bd8d3598be553801ec2931b34ca95ae2d2d
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-18 07:49:22 +0200

    bon dark/light mode fonctionnel. Bug logo lightmod

--- js/mode.js
+++ js/mode.js
@@ -1,6 +1,11 @@
 let link = document.querySelector(".link");
 let lightbtn = document.querySelector(".lightmode");
+let darkbtn = document.querySelector(".darkmode")
 
 lightbtn.addEventListener("click", () =>{
-    link.href = "css/lightmode.css";
+    link.href = "css/style.css";
+})
+
+darkbtn.addEventListener("click", () =>{
+    link.href= "css/dark.css";
 })
