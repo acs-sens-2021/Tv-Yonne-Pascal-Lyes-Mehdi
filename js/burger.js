985ea2eef7ac187d31581f648a72ea77b23682c5
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-20 14:00:32 +0200

    fin burger

--- js/burger.js
+++ js/burger.js
@@ -2,17 +2,24 @@ let burger = document.querySelector(".burger");
 let menu = document.querySelector(".lien");
 let nav = document.querySelector(".nav-main");
 let closebtn = document.querySelector(".closebtn");
+let closebtndark = document.querySelector(".closebtndark");
 let body = document.querySelector("main");
 
 burger.addEventListener("click", () => {
     menu.style.display = "flex";
     burger.style.display = "none";
     closebtn.style.display = "block";
+    closebtndark.style.display ="block";
 });
 
 closebtn.addEventListener("click", () => {
     menu.style.display = "none";
     closebtn.style.display = "none";
+    burger.style.display = "flex";
+});
+closebtndark.addEventListener("click", () => {
+    menu.style.display = "none";
+    closebtndark.style.display = "none";
     burger.style.display = "flex";
 });
 
