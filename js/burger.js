be35c689256e2d5462446f9e08a53024502790cf
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-21 15:14:32 +0200

    ajout croix burger + bug navbar

--- js/burger.js
+++ js/burger.js
@@ -1,25 +1,18 @@
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".lien");
 let nav = document.querySelector(".nav-main");
-let closebtn = document.querySelector(".closebtn");
-let closebtndark = document.querySelector(".closebtndark");
+let closebtn = document.querySelector(".close");
 let body = document.querySelector("main");
 
 burger.addEventListener("click", () => {
     menu.style.display = "flex";
     burger.style.display = "none";
-    closebtn.style.display = "block";
-    closebtndark.style.display ="block";
+    closebtn.style.display="flex";
 });
 
 closebtn.addEventListener("click", () => {
     menu.style.display = "none";
     closebtn.style.display = "none";
-    burger.style.display = "flex";
-});
-closebtndark.addEventListener("click", () => {
-    menu.style.display = "none";
-    closebtndark.style.display = "none";
     burger.style.display = "flex";
 });
 
--- js/mode.js
+++ js/mode.js
@@ -1,6 +1,7 @@
 let link = document.querySelector(".link");
 let lightbtn = document.querySelector(".lightmode");
 let darkbtn = document.querySelector(".darkmode")
+let script = document.querySelector("#script");
 
 lightbtn.addEventListener("click", () =>{
     link.href = "css/style.css";
